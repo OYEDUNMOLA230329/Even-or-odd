c421b4f4084d73f94f171e4619b2da6377f55eb4
OYEDUNMOLA230329 <alaooyedunmola2022@gmail.com>
2024-08-28 13:27:55 +0100

    Add files via upload

---
+++ EvenOrOddNumber.java
@@ -0,0 +1,19 @@
+
+package even.or.odd.number;
+import java.util.Scanner;
+
+public class EvenOrOddNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+      System.out.print("Enter a number");
+      int num = scanner.nextInt();
+      
+      if (num % 2 ==0){
+      System.out.println(num +" is an even number");
+      }else{
+          System.out.println(num +" is an odd number");
+           }
+         scanner.close();
+    }
+}
